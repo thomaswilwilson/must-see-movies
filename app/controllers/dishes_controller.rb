ac95bc50d8b98a680549e013b56ba86a8c61e7b8
Brendan Sanderson <brendansanderson@uchicago.edu>
2018-03-08 22:13:14 +0000

    finished dishes page

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,14 +2,12 @@ class DishesController < ApplicationController
   def index
     @q = Dish.ransack(params[:q])
     @dishes = @q.result(:distinct => true).includes(:cuisine, :bookmarks, :fans, :specialists).page(params[:page]).per(10)
-
     render("dishes/index.html.erb")
   end
 
   def show
     @bookmark = Bookmark.new
     @dish = Dish.find(params[:id])
-
     render("dishes/show.html.erb")
   end
 
